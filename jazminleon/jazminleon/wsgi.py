62e6af054dd51c934b27c93f5efd3616fbabe252
Joanne Jordan <joanne.k.m.jordan@gmail.com>
2019-06-01 23:24:29 +0000

    made changes

--- jazminleon/jazminleon/wsgi.py
+++ jazminleon/jazminleon/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jazminleon.settings.prod")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jazminleon.settings")
 
 application = get_wsgi_application()
